465a00cac12954478c65e7a222af7020f9aa6167
kakha <k_kareanshvili@cu.edu.ge>
2025-07-13 14:36:50 +0400

    pushing missed out dto

---
+++ Services/MainApp/MainApp.Application/Dto/Response/GetSubmissionResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.Common.Enums;
+
+namespace MainApp.Application.Dto.Response;
+
+public class GetSubmissionResponseDto
+{
+    public int Id { get; set; }
+    public string AuthUsername { get; set; }
+    public string Language { set; get; }
+    public string Code { get; set; }
+    public int ProblemId { get; set; }
+    public string ProblemName { get; set; }
+    public string Status { get; set; }
+    public string? Input { set; get; }
+    public string? ExpectedOutput { set; get; }
+    public string? Output { set; get; }
+}
