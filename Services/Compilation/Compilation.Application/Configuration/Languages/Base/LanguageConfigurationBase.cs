044607cbce97c674fb4834d22db04850307bf868
kakha <k_kareanshvili@cu.edu.ge>
2025-08-22 16:13:42 +0400

    added process profiles for different languages

---
+++ Services/Compilation/Compilation.Application/Configuration/Languages/Base/LanguageConfigurationBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace Compilation.Application.Configuration.Languages.Base;
+
+public abstract class LanguageConfigurationBase
+{
+    public abstract string Name { get; }
+    public abstract string FileExtension { get; }
+    public abstract bool RequiresCompilation { get; }
+    public abstract string GetFileName(string fileId);
+    public abstract string GetExecutableFileName(string fileId);
+    public abstract ProcessStartInfo GetCompileProcessStartInfo(string sourceFileName,string executableFileName);
+    public abstract ProcessStartInfo GetExecuteProcessStartInfo(string executableFileName);
+    
+}
---
+++ Services/Compilation/Compilation.Application/Configuration/Languages/CppConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Compilation.Application.Configuration.Languages.Base;
+
+namespace Compilation.Application.Configuration.Languages;
+
+public class CppConfiguration : LanguageConfigurationBase
+{
+    public override string Name => "C++";
+    public override string FileExtension => ".cpp";
+    public override bool RequiresCompilation => true;
+
+    public override string GetFileName(string fileId) => $"cpp-file_{fileId}.cpp";
+
+    public override string GetExecutableFileName(string fileId)=> $"cpp-file_{fileId}.exe";
+
+    public override ProcessStartInfo GetCompileProcessStartInfo(string sourceFileName, string executableFileName)
+    {
+        return new ProcessStartInfo
+        {
+            FileName = "g++",
+            ArgumentList = {sourceFileName, "-o", executableFileName},
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true,
+        };
+    }
+
+    public override ProcessStartInfo GetExecuteProcessStartInfo(string executableFileName)
+    {
+        return new ProcessStartInfo
+        {
+            FileName = executableFileName,
+            UseShellExecute = false,
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true,
+        };
+        
+    }
+    
+}
