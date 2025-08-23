0cc7146e67c80a4d24a51733d9a7f4e4ceb73074
kakha <k_kareanshvili@cu.edu.ge>
2025-08-23 23:40:53 +0400

    bugfix service in added DependencyInjection.cs

--- Services/Compilation/Compilation.Application/DependencyInjection.cs
+++ Services/Compilation/Compilation.Application/DependencyInjection.cs
@@ -9,8 +9,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplicationServices
         (this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddScoped<CppTestingService>();
-        services.AddScoped<PythonTestingService>();
+        services.AddScoped<CodeTestingService>();
         services.AddTransient<MemoryMonitorService>();
         
         services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
